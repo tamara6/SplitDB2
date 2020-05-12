57da95358b6efccd4a891c29ee814319c9b70692
tamara6 <tsnyder@uark.edu>
2020-05-12 10:38:30 -0500

    Initial Commit

---
+++ SplitDB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SplitDB
+//
+//  Created by Tamara Snyder on 5/12/20.
+//  Copyright © 2020 Tamara Snyder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SplitDB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SplitDB
+//
+//  Created by Tamara Snyder on 5/12/20.
+//  Copyright © 2020 Tamara Snyder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
